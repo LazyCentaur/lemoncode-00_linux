3d9d82e435d59d2d24f6f0ec21d098d53b4ce406
LazyCentaur <92822545+LazyCentaur@users.noreply.github.com>
2023-09-26 12:42:21 +0200

    Update ejercicio2.sh

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+text="Que me gusta la bash!!!!"
+
+echo "Introoduce un texto:"
+read textUser
+
+if [[ $textUser -gt 0 ]]
+then
+  text=textUser
+fi
+
+mkdir dummy
+mkdir empty
+cd dummy
+echo $text > file1.txt
+touch file2.txt
+cp file1.txt file2.txt 
+mv file2.txt ../empty
